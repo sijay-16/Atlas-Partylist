8c35f43f97ab955253685210f1e02cfc832e7270
sijay-16 <123277660+sijay-16@users.noreply.github.com>
2023-06-11 14:51:17 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 var members = document.getElementById('members')
 
-var member = 5;
+add('Raquel A. Soriano', 'President', 'raquel')
+add('Alexa C. Miranda', 'Vice President', 'alexa')
+add('Daniela O. Burzon', 'Secretary', 'daniela')
 
-for (var i = 0; i < member; i++) {
+function add(Name, Position, classList) {
   var listItem = document.createElement("li")
   var div = document.createElement('div')
+  var name = document.createElement('h1')
+  var position = document.createElement('p')
+  name.innerText = Name
+  position.innerText = Position
+  name.classList.add('name')
+  position.classList.add('position')
   div.classList.add('profile')
+  div.classList.add(classList)
+  listItem.appendChild(name)
+  listItem.appendChild(position)
   listItem.appendChild(div)
   listItem.classList.add("member")
   members.appendChild(listItem)
 }
 
-console.log(members);
+console.log(profile);
